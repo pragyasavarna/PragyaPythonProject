536860b21a57b6b3548e8b39daf359f91667a89e
pragyasavarna <savarnapragya181751@gmail.com>
2025-11-27 18:11:46 +0530

    Updated Project Page

--- ImageWebsite/AIModel/Model/Visual_Representation.py
+++ ImageWebsite/AIModel/Model/Visual_Representation.py
@@ -17,7 +17,7 @@ model_dir = os.path.join(BASE_DIR, "Model/"+file_name)
 print(model_dir)
 
 
-url = "http://127.0.0.1:8000/AIModel/history.pkl"
+url = "http://127.0.0.1:8000/AIModel/animal_history.pkl"
 response = requests.get(url)
 response.raise_for_status()
 history_dict = pickle.loads(response.content)
@@ -34,7 +34,7 @@ def save_plot(columns, title, filename):
     plt.savefig(path, bbox_inches="tight")
     plt.close(fig)
 
-save_plot(['loss', 'val_loss'], "Model Loss", "loss.png")
-save_plot(['accuracy', 'val_accuracy'], "Model Accuracy", "accuracy.png")
+save_plot(['loss', 'val_loss'], "Model Loss", "animal_loss.png")
+save_plot(['accuracy', 'val_accuracy'], "Model Accuracy", "animal_accuracy.png")
 
 print("✅ Plots saved in static/plots/")
