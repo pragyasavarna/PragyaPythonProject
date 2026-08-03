3f6c87fd90f4bc026ece8c97e005ef6f0228c5d2
pragyasavarna <savarnapragya181751@gmail.com>
2026-08-03 23:58:29 +0530

    login register design changes

--- ImageWebsite/HtmlWebsite/JavaScript/auth.js
+++ ImageWebsite/HtmlWebsite/JavaScript/auth.js
@@ -5,6 +5,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Make sure the element exists before adding the listener
     if (dobInput) {
+        if (window.innerWidth <= 768) {
+            // Get today's date and format it strictly as YYYY-MM-DD
+            const today = new Date().toISOString().split('T')[0];
+            dobInput.value = today; // Set the default value
+        }
         dobInput.addEventListener('click', function () {
             // Check if the browser supports showPicker()
             if (this.showPicker) {
